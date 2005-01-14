e9983a0593da85cf1588230c1775ef2fa1af14e5
Paul Cochrane <paultcochrane@gmail.com>
2005-01-14 05:06:23 +0000

    One needs to pass the scene object to the Plot objects to associate that object with the Scene object (this also helps to pass other info around).  Implemented passing Scene object to Plot objects, and added docstrings for the __init__ functions.  Also added some properties such as title and xlabel to the LinePlot class.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@130 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/pyvisi/renderers/gnuplot/plot.py
+++ trunk/pyvisi/renderers/gnuplot/plot.py
@@ -19,7 +19,7 @@
 ## @file plot.py
 
 """
-Class and functions associated with a pyvisi Plot objects
+Class and functions associated with a pyvisi Plot objects (gnuplot)
 """
 
 # generic imports
@@ -33,9 +33,14 @@ class Plot(Scene):
     """
     Abstract plot class
     """
-    def __init__(self):
+    def __init__(self, scene):
+        """
+        Initialisation of abstract Plot class
+
+        @param scene: the scene with which to associate the Plot
+        """
         if _debug: print "\t%s: Called Plot.__init__()" % rendererName
-        pass
+        return
 
     def setData(self,data):
         if _debug: print "\t%s: Called setData() in Plot()" % rendererName
@@ -45,7 +50,12 @@ class ArrowPlot(Plot):
     """
     Arrow field plot
     """
-    def __init__(self):
+    def __init__(self,scene):
+        """
+        Initialisation of ArrowPlot class
+
+        @param scene: the scene with which to associate the ArrowPlot
+        """
         if _debug: print "\t%s: Called ArrowPlot.__init__()" % rendererName
         pass
 
@@ -57,7 +67,12 @@ class ContourPlot(Plot):
     """
     Contour plot
     """
-    def __init__(self):
+    def __init__(self,scene):
+        """
+        Initialisation of ContourPlot class
+
+        @param scene: the scene with which to associate the ContourPlot
+        """
         if _debug: print "\t%s: Called ContourPlot.__init__()" % rendererName
         pass
 
@@ -69,12 +84,36 @@ class LinePlot(Plot):
     """
     Line plot
     """
-    def __init__(self):
-        if _debug: print "\t%s: Called LinePlot.__init__()" % rendererName
-        pass
+    def __init__(self,scene):
+        """
+        Initialisation of LinePlot class
 
-    def setData(self,data):
+        @param scene: the scene with which to associate the LinePlot
+        """
+        if _debug: print "\t%s: Called LinePlot.__init__()" % rendererName
+
+        self.renderer.addToEvalStack("# LinePlot.__init__()")
+
+        self.title = None
+        self.xlabel = None
+        self.ylabel = None
+        self.zlabel = None
+
+        return
+
+    def setData(self,**data):
+        """
+        Sets the data to the given plot object.
+
+        @param data: list of data objects to plot
+        """
         if _debug: print "\t%s: Called setData() in LinePlot()" % rendererName
+        
+        self.renderer.addToEvalStack("# LinePlot.setData()")
+        dataLen = len(data)
+        print "dataLen = %s" % dataLen
+        self.renderer.addToEvalStack("_data = Gnuplot.Data(x, y1)")
+
         return True
 
 # vim: expandtab shiftwidth=4:
