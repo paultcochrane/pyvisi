3989e3d70175f6b5d5215c45b9bb0fd605603822
Paul Cochrane <paultcochrane@gmail.com>
2005-01-21 06:14:46 +0000

    Corrected filename in docstring.  Corrected the fact that are using three methods rather than two.  Other minor textual changes.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@221 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/plotExample.py
+++ trunk/examples/plotExample.py
@@ -16,7 +16,7 @@
 
 # $Id$
 
-## @file basicExample.py
+## @file plotExample.py
 
 """
 Example of plotting with pyvisi 
@@ -36,15 +36,15 @@ from Numeric import *
 x = arange(10, typecode=Float)
 y = x**2
 
-# plot it using one of the two methods
+# plot it using one of the three methods
 if method == 'pyvisi':
 
     # example code for how a user would write a script in pyvisi
     from pyvisi import *          # base level visualisation stuff
     #from pyvisi.utils import *   # pyvisi specific utils
     # import the objects to render the scene using the specific renderer
-    #from pyvisi.renderers.gnuplot import *   # gnuplot
-    from pyvisi.renderers.vtk import *       # vtk
+    from pyvisi.renderers.gnuplot import *   # gnuplot
+    #from pyvisi.renderers.vtk import *       # vtk
     
     # define the scene object
     # a Scene is a container for all of the kinds of things you want to put 
