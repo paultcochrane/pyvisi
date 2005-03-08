d0a1237111aa0827b7e79e766eeb363f3eac5760
Paul Cochrane <paultcochrane@gmail.com>
2005-03-08 01:07:45 +0000

    Added and implemented the MeshPlot class.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@540 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/pyvisi/renderers/gnuplot/plot.py
+++ trunk/pyvisi/renderers/gnuplot/plot.py
@@ -621,6 +621,144 @@ class LinePlot(Plot):
 
         return self.linestyle
 
+class MeshPlot(Plot):
+    """
+    Mesh plot
+    """
+
+    def __init__(self, scene):
+        """
+        Initialisation of MeshPlot class
+
+        @param scene: the scene with which to associate the MeshPlot
+        @type scene: Scene object
+        """
+        debugMsg("Called MeshPlot.__init__()")
+        Plot.__init__(self, scene)
+
+        # grab the renderer
+        self.renderer = scene.renderer
+
+        # set up some of the attributes
+        self.title = None
+        self.xlabel = None
+        self.ylabel = None
+        self.zlabel = None
+
+        # to show contours of the surface on the bottom of the axes, set
+        # this variable to True
+        self.contours = False
+
+        # now add the object to the scene
+        scene.add(self)
+
+    def setData(self, *dataList):
+        """
+        Sets the data to the given plot object.
+
+        @param dataList: list of data objects to plot
+        @type dataList: tuple
+        """
+        debugMsg("Called setData() in MeshPlot()")
+
+        self.renderer.addToEvalStack("# MeshPlot.setData()")
+
+        # for the moment, make sure that there are three arrays
+        if len(dataList) != 3:
+            raise ValueError, "Must have three arrays as input (at present)"
+
+        # do some sanity checks on the data
+        xData = dataList[0]
+        yData = dataList[1]
+        zData = dataList[2]
+
+        if len(xData.shape) != 1:
+            raise ValueError, "x data array is not of the correct shape: %s"\
+                    % xData.shape
+
+        if len(yData.shape) != 1:
+            raise ValueError, "y data array is not of the correct shape: %s"\
+                    % yData.shape
+
+        if len(zData.shape) != 2:
+            raise ValueError, "z data array is not of the correct shape: %s"\
+                    % zData.shape
+
+        # range over the data, printing what the expansion of the array is
+        # and regenerate the data within the eval
+        ## the x data
+        evalString = "_x = ["
+        for j in range(len(xData)-1):
+            evalString += "%s, " % xData[j]
+        evalString += "%s]" % xData[-1]
+        self.renderer.addToEvalStack(evalString)
+
+        ## the y data
+        evalString = "_y = ["
+        for j in range(len(yData)-1):
+            evalString += "%s, " % yData[j]
+        evalString += "%s]" % yData[-1]
+        self.renderer.addToEvalStack(evalString)
+
+        ## the z data
+        evalString = "_z = ["
+        for i in range(len(xData)):
+            evalString += "["
+            for j in range(len(yData)-1):
+                evalString += "%s, " % zData[i, j]
+            evalString += "%s],\n" % zData[i, -1]
+        evalString += "]"
+        self.renderer.addToEvalStack(evalString)
+
+        self.renderer.addToEvalStack(\
+                "_data = Gnuplot.GridData(_z, _x, _y, binary=1)")
+
+        return
+
+    def render(self):
+        """
+        Does MeshPlot object specific rendering stuff
+        """
+        debugMsg("Called MeshPlot.render()")
+
+        self.renderer.addToEvalStack("# MeshPlot.render()")
+        self.renderer.addToEvalStack("_gnuplot('set surface')")
+
+        # if a title is set, put it here
+        if self.title is not None:
+            evalString = "_gnuplot.title(\'%s\')" % self.title
+            self.renderer.addToEvalStack(evalString)
+
+        # if an xlabel is set, add it
+        if self.xlabel is not None:
+            evalString = "_gnuplot.xlabel(\'%s\')" % self.xlabel
+            self.renderer.addToEvalStack(evalString)
+
+        # if a ylabel is set add it
+        if self.ylabel is not None:
+            evalString = "_gnuplot.ylabel(\'%s\')" % self.ylabel
+            self.renderer.addToEvalStack(evalString)
+
+        # if a zlabel is set add it
+        if self.zlabel is not None:
+            evalString = "_gnuplot('set zlabel \\'%s\\'')" % self.zlabel
+            self.renderer.addToEvalStack(evalString)
+
+        if not _gnuplot4:
+            evalString = "_gnuplot('set data style lines')"
+            self.renderer.addToEvalStack(evalString)
+
+        # if contours is true, set the relevant option
+        if self.contours:
+            evalString = "_gnuplot('set contour base')"
+            self.renderer.addToEvalStack(evalString)
+
+        # set up the evalString to use for plotting
+        evalString = "_gnuplot.splot(_data)"
+        self.renderer.addToEvalStack(evalString)
+
+        return
+         
 class ScatterPlot(Plot):
     """
     Scatter plot 
