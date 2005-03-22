e1a8765739be74d3b95ae45c479c3e41fc28bb9b
Paul Cochrane <paultcochrane@gmail.com>
2005-03-22 05:55:32 +0000

    Added vtk-specific implementation to show what's involved for that renderer.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@569 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/offsetLinePlotExample.py
+++ trunk/examples/offsetLinePlotExample.py
@@ -129,7 +129,37 @@ elif method == 'vtk':
     if len(x) != len(y2):
         raise ValueError, "x and y vectors must be same length"
 
-    # set up the x and y data arrays to be able to accept the data (code
+    if len(x) != len(y3):
+        raise ValueError, "x and y vectors must be same length"
+
+    if len(x) != len(y4):
+        raise ValueError, "x and y vectors must be same length"
+
+    if len(x) != len(y5):
+        raise ValueError, "x and y vectors must be same length"
+
+    if len(x) != len(y6):
+        raise ValueError, "x and y vectors must be same length"
+
+    ### set up the data to be offset
+    # concatenate the data
+    yAll = concatenate( [y1, y2, y3, y4, y5, y6] )
+
+    yMax = max(yAll)
+    yMin = min(yAll)
+
+    # keep the data apart a bit
+    const = 0.1*(yMax-yMin)
+
+    # don't need to worry about y1: it's the first data series
+    shift = yMax - yMin + const
+    y2 = y2 + shift
+    y3 = y3 + 2*shift
+    y4 = y4 + 3*shift
+    y5 = y5 + 4*shift
+    y6 = y6 + 5*shift
+
+   # set up the x and y data arrays to be able to accept the data (code
     # here adapted from the C++ of a vtk-users mailing list reply by Sander
     # Niemeijer)
     _xData = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
@@ -144,12 +174,24 @@ elif method == 'vtk':
     _yData3 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
     _yData3.SetNumberOfTuples(len(y3))
 
+    _yData4 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
+    _yData4.SetNumberOfTuples(len(y4))
+
+    _yData5 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
+    _yData5.SetNumberOfTuples(len(y5))
+
+    _yData6 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
+    _yData6.SetNumberOfTuples(len(y6))
+
     # put the data into the data arrays
     for i in range(len(x)):
         _xData.SetTuple1(i,x[i])
         _yData1.SetTuple1(i,y1[i])
         _yData2.SetTuple1(i,y2[i])
         _yData3.SetTuple1(i,y3[i])
+        _yData4.SetTuple1(i,y4[i])
+        _yData5.SetTuple1(i,y5[i])
+        _yData6.SetTuple1(i,y6[i])
 
     # create a field data object 
     # (I think this is as a containter to hold the data arrays)
@@ -168,6 +210,21 @@ elif method == 'vtk':
     _fieldData3.AddArray(_xData)
     _fieldData3.AddArray(_yData3)
 
+    _fieldData4 = vtk.vtkFieldData()
+    _fieldData4.AllocateArrays(2)
+    _fieldData4.AddArray(_xData)
+    _fieldData4.AddArray(_yData4)
+
+    _fieldData5 = vtk.vtkFieldData()
+    _fieldData5.AllocateArrays(2)
+    _fieldData5.AddArray(_xData)
+    _fieldData5.AddArray(_yData5)
+
+    _fieldData6 = vtk.vtkFieldData()
+    _fieldData6.AllocateArrays(2)
+    _fieldData6.AddArray(_xData)
+    _fieldData6.AddArray(_yData6)
+
     # now put the field data object into a data object so that can add it as
     # input to the xyPlotActor
     _dataObject1 = vtk.vtkDataObject()
@@ -179,11 +236,23 @@ elif method == 'vtk':
     _dataObject3 = vtk.vtkDataObject()
     _dataObject3.SetFieldData(_fieldData3)
 
+    _dataObject4 = vtk.vtkDataObject()
+    _dataObject4.SetFieldData(_fieldData4)
+
+    _dataObject5 = vtk.vtkDataObject()
+    _dataObject5.SetFieldData(_fieldData5)
+
+    _dataObject6 = vtk.vtkDataObject()
+    _dataObject6.SetFieldData(_fieldData6)
+
     # set up the actor
     _plot = vtk.vtkXYPlotActor()
     _plot.AddDataObjectInput(_dataObject1)
     _plot.AddDataObjectInput(_dataObject2)
     _plot.AddDataObjectInput(_dataObject3)
+    _plot.AddDataObjectInput(_dataObject4)
+    _plot.AddDataObjectInput(_dataObject5)
+    _plot.AddDataObjectInput(_dataObject6)
 
     # set the title and stuff
     _plot.SetTitle("Example 2D plot with offsets")
@@ -196,6 +265,9 @@ elif method == 'vtk':
     _plot.SetDataObjectYComponent(0,1)
     _plot.SetDataObjectYComponent(1,1)
     _plot.SetDataObjectYComponent(2,1)
+    _plot.SetDataObjectYComponent(3,1)
+    _plot.SetDataObjectYComponent(4,1)
+    _plot.SetDataObjectYComponent(5,1)
 
     # add the actor
     _ren.AddActor2D(_plot)
