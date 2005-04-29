0e8a218e295d140011a1f10a12846bf7192ab3d0
Paul Cochrane <paultcochrane@gmail.com>
2005-04-29 00:32:20 +0000

    Removed copyright info and superfluous other comments.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@602 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/basicContourExample.py
+++ trunk/examples/basicContourExample.py
@@ -1,22 +1,4 @@
-# Copyright (C) 2004 Paul Cochrane
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
 # $Id$
-
-## @file basicContourExample.py
 
 """
 Example of a basic contour plot
@@ -25,9 +7,6 @@ Will hopefully help me write a decent interface.
 """
 
 import sys,os
-
-# this means that one can run the script from the examples directory
-sys.path.append('../')
 
 # import the python visualisation interface
 from pyvisi import *
