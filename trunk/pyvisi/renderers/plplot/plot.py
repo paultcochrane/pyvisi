53ca457a63987cdf43b7b41519b5a5b707762850
Paul Cochrane <paultcochrane@gmail.com>
2005-05-05 01:53:10 +0000

    Added an incomplete implementation of contour plots.  Added a basic implementation of surface plots.  Fixed some docstrings.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@616 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/pyvisi/renderers/plplot/plot.py
+++ trunk/pyvisi/renderers/plplot/plot.py
@@ -19,7 +19,7 @@
 ## @file plot.py
 
 """
-Brief introduction to what the file contains/does
+This file contains all of the classes for the various plotting objects.
 """
 
 from pyvisi.renderers.plplot.common import debugMsg
@@ -31,7 +31,7 @@ __revision__ = '$Revision$'
     
 class Plot(Item):
     """
-    Brief introduction to what the class does
+    Abstract plot class
     """
 
     def __init__(self, scene):
@@ -159,22 +159,144 @@ class ArrowPlot(Plot):
     
 class ContourPlot(Plot):
     """
-    Brief introduction to what the class does
+    Contour Plots
     """
 
-    def __init__(self, arg):
+    def __init__(self, scene):
         """
-        Brief description of the init function
+        Initialisation of the ContourPlot class
 
-        @param arg: a description of the argument
-        @type arg: the type of the argument
+        @param arg: The scene with which to associate the plot
+        @type arg: Scene object
         """
         debugMsg("Called ContourPlot.__init__()")
         Plot.__init__(self)  # initialisation of base class
+        
+        self.renderer = scene.renderer
+
+        self.title = None
+        self.xlabel = None
+        self.ylabel = None
+        self.zlabel = None
+
+        self.linestyle = None  # pyvisi-defined linestyle
+        self._linestyle = None # renderer-specific linestyle
     
+        # now add the object to the scene
+        scene.add(self)
+
+    def setData(self, *dataList, **options):
+        """
+        Sets the data to the given plot object.
+
+        @param dataList: list of data objects to plot
+        @type dataList: tuple
+        """
+        debugMsg("Called setData() in ContourPlot()")
+
+        self.renderer.addToEvalStack("# ContourPlot.setData()")
+
+        # this is a really dodgy way to get the data into the renderer
+        # I really have to find a better, more elegant way to do this
+
+        # for the moment, make sure that there are three arrays
+        if len(dataList) != 3:
+            raise ValueError, "Must have three arrays as input (at present)"
+
+        # do some sanity checks on the data
+        xData = dataList[0]
+        yData = dataList[1]
+        zData = dataList[2]
+
+        if len(xData.shape) != 1:
+            raise ValueError, "x data array is not of correct shape: %s" % \
+                    xData.shape
+
+        if len(yData.shape) != 1:
+            raise ValueError, "y data array is not of correct shape: %s" % \
+                    yData.shape
+
+        if len(zData.shape) != 2:
+            raise ValueError, "z data array is not of correct shape: %s" % \
+                    zData.shape
+
+        # range over the data, printing what the expansion of the array is
+        # and regenerate the data within the eval
+        ## the x data
+        evalString = "_x = array(["
+        for j in range(len(xData)-1):
+            evalString += "%s, " % xData[j]
+        evalString += "%s])" % xData[-1]
+        self.renderer.addToEvalStack(evalString)
+
+        ## the y data
+        evalString = "_y = array(["
+        for j in range(len(yData)-1):
+            evalString += "%s, " % yData[j]
+        evalString += "%s])" % yData[-1]
+        self.renderer.addToEvalStack(evalString)
+
+        ## the z data
+        evalString = "_z = array(["
+        for i in range(len(xData)):
+            evalString += "["
+            for j in range(len(yData)-1):
+                evalString += "%s, " % zData[i, j]
+            evalString += "%s],\n" % zData[i, -1]
+        evalString += "])"
+        self.renderer.addToEvalStack(evalString)
+
+        # determine the min and max of x, y and z
+        evalString = "_xMin = min(x)\n"
+        evalString += "_xMax = max(x)\n"
+        evalString += "_yMin = min(y)\n"
+        evalString += "_yMax = max(y)\n"
+        evalString += "_zMin = \n"
+        evalString += "_zMax = \n"
+
+        ### please complete me!!
+
+        self.renderer.addToEvalStack(evalString)
+
+        return
+
+    def render(self):
+        """
+        Does ContourPlot object specific rendering stuff
+        """
+        debugMsg("Called ContourPlot.render()")
+
+        self.renderer.addToEvalStack("# ContourPlot.render()")
+        self.renderer.addToEvalStack("_gnuplot('set contour base')")
+        self.renderer.addToEvalStack("_gnuplot('set view 0, 0, 1, 1')")
+        self.renderer.addToEvalStack("_gnuplot('set nosurface')") # gnuplot 3.7
+
+         # if a title is set, put it here
+        if self.title is not None:
+            evalString = "_gnuplot.title(\'%s\')" % self.title
+            self.renderer.addToEvalStack(evalString)
+
+        # if an xlabel is set, add it
+        if self.xlabel is not None:
+            evalString = "_gnuplot.xlabel(\'%s\')" % self.xlabel
+            self.renderer.addToEvalStack(evalString)
+
+        # if a ylabel is set, add it
+        if self.ylabel is not None:
+            evalString = "_gnuplot.ylabel(\'%s\')" % self.ylabel
+            self.renderer.addToEvalStack(evalString)
+
+        self.renderer.addToEvalStack("_gnuplot('set pm3d')")
+
+        # set up the evalString to use for plotting
+        evalString = "_gnuplot.splot(_data)"
+        self.renderer.addToEvalStack(evalString)
+
+        return
+
 class LinePlot(Plot):
     """
-    LinePlots
+    Line Plots
     """
 
     def __init__(self, scene):
@@ -209,6 +331,9 @@ class LinePlot(Plot):
 
         @param dataList: list of data objects to plot
         @type dataList: tuple
+
+        @param options: keyword options
+        @type options: dict
         """
         debugMsg("Called setData() in LinePlot()")
         
@@ -429,4 +554,187 @@ class LinePlot(Plot):
 
         return self.linestyle
 
+
+class SurfacePlot(Plot):
+    """
+    Surface plot
+    """
+
+    def __init__(self, scene):
+        """
+        Initialisation of SurfacePlot class
+
+        @param scene: the scene with which to associate the SurfacePlot
+        @type scene: Scene object
+        """
+        debugMsg("Called SurfacePlot.__init__()")
+        Plot.__init__(self, scene)
+
+        # grab the renderer
+        self.renderer = scene.renderer
+
+        # set up some of the attributes
+        self.title = None
+        self.xlabel = None
+        self.ylabel = None
+        self.zlabel = None
+
+        # to show contours of the surface on the bottom of the axes, set
+        # this variable to True
+        self.contours = False
+
+        # now add the object to the scene
+        scene.add(self)
+
+    def setData(self, *dataList):
+        """
+        Sets the data to the given plot object.
+
+        @param dataList: list of data objects to plot
+        @type dataList: tuple
+        """
+        debugMsg("Called setData() in SurfacePlot()")
+
+        self.renderer.addToEvalStack("# SurfacePlot.setData()")
+
+        # for the moment, make sure that there are three arrays
+        if len(dataList) != 3:
+            raise ValueError, "Must have three arrays as input (at present)"
+
+        # do some sanity checks on the data
+        xData = dataList[0]
+        yData = dataList[1]
+        zData = dataList[2]
+
+        if len(xData.shape) != 1:
+            raise ValueError, "x data array is not of the correct shape: %s"\
+                    % xData.shape
+
+        if len(yData.shape) != 1:
+            raise ValueError, "y data array is not of the correct shape: %s"\
+                    % yData.shape
+
+        if len(zData.shape) != 2:
+            raise ValueError, "z data array is not of the correct shape: %s"\
+                    % zData.shape
+
+        # range over the data, printing what the expansion of the array is
+        # and regenerate the data within the eval
+        ## the x data
+        evalString = "_x = array(["
+        for j in range(len(xData)-1):
+            evalString += "%s, " % xData[j]
+        evalString += "%s])" % xData[-1]
+        self.renderer.addToEvalStack(evalString)
+
+        ## the y data
+        evalString = "_y = array(["
+        for j in range(len(yData)-1):
+            evalString += "%s, " % yData[j]
+        evalString += "%s])" % yData[-1]
+        self.renderer.addToEvalStack(evalString)
+
+        ## the z data
+        evalString = "_z = array(["
+        for i in range(len(xData)):
+            evalString += "["
+            for j in range(len(yData)-1):
+                evalString += "%s, " % zData[i, j]
+            evalString += "%s],\n" % zData[i, -1]
+        evalString += "])"
+        self.renderer.addToEvalStack(evalString)
+
+        # determine the min and max of x, y and z in world coordinates
+        evalString = "_xMin = min(_x)\n"
+        evalString += "_xMax = max(_x)\n"
+        evalString += "_yMin = min(_y)\n"
+        evalString += "_yMax = max(_y)\n"
+        evalString += "_zMin = min(_z.flat)\n"
+        evalString += "_zMax = max(_z.flat)"
+        self.renderer.addToEvalStack(evalString)
+
+        # min and max of x and y variables in normalised coordinates
+        # values are those recommended in plplot documentation for surface
+        # plots, and are hardcoded here for the meantime.
+        evalString = "_xMin2D = -2.5\n"
+        evalString += "_xMax2D = 2.5\n"
+        evalString += "_yMin2D = -2.5\n"
+        evalString += "_yMax2D = 4.0"
+        self.renderer.addToEvalStack(evalString)
+
+        # sides of the box in normalised coordinates, again, these are those
+        # recommended by plplot
+        evalString = "_basex = 2.0\n"
+        evalString += "_basey = 4.0\n"
+        evalString += "_height = 3.0"    # possible name clash???
+        self.renderer.addToEvalStack(evalString)
+
+        # the angle to view the box (this needs to be set up by a Camera()
+        # object in the future)
+        evalString = "_alt = 45.0\n"
+        evalString += "_az = 30.0"
+        self.renderer.addToEvalStack(evalString)
+
+        return
+
+    def render(self):
+        """
+        Does SurfacePlot object specific rendering stuff
+        """
+        debugMsg("Called SurfacePlot.render()")
+
+        self.renderer.addToEvalStack("# SurfacePlot.render()")
+        # initialise plplot 
+        self.renderer.addToEvalStack("plplot.plinit()")
+
+        # set up the viewport for plotting
+        evalString = "plplot.plenv(_xMin2D,_xMax2D,_yMin2D,_yMax2D, 0, -2)"
+        self.renderer.addToEvalStack(evalString)
+
+        # set up the window
+        evalString = "plplot.plw3d(_basex, _basey, _height, "
+        evalString += "_xMin, _xMax, _yMin, _yMax, _zMin, _zMax, "
+        evalString += "_alt, _az)"
+        self.renderer.addToEvalStack(evalString)
+
+        # if a title is not set, set it to a null string
+        # (this will help keep plplot happy)
+        if self.title is not None:
+            evalString = "plplot.plmtex(\"t\", 1.0, 0.5, 0.5, \"%s\")" % \
+                    self.title
+            self.renderer.addToEvalStack(evalString)
+
+        # if an xlabel is not set, set it to a null string
+        if self.xlabel is None:
+            self.xlabel = ""
+
+        # if a ylabel is not set, set it to a null string
+        if self.ylabel is None:
+            self.ylabel = ""
+
+        # if a zlabel is not set, set it to a null string
+        if self.zlabel is None:
+            self.zlabel = ""
+
+        # put the labels (if any) on the graph.
+        evalString = "plplot.plbox3(\"bnstu\", \"%s\", 0.0, 0, " % self.xlabel
+        evalString += "\"bnstu\", \"%s\", 0.0, 0, " % self.ylabel
+        evalString += "\"bcdmnstuv\", \"%s\", 0.0, 0)" % self.zlabel
+        self.renderer.addToEvalStack(evalString)
+
+        # plot it!
+        ### note: I can put other shading options into this call
+        evalString = "plplot.plsurf3d(_x, _y, _z, 0, ())"
+        self.renderer.addToEvalStack(evalString)
+
+        # finish stuff off
+        self.renderer.addToEvalStack("plplot.plend()")
+
+        # if contours is true, set the relevant option
+        if self.contours:
+            pass
+            # I need to implement this!!!  And put it further up in render()
+
+        return
+         
 # vim: expandtab shiftwidth=4:
