93c13ce6d52f575ad25147bda39c9839206d806f
Paul Cochrane <paultcochrane@gmail.com>
2005-05-24 01:36:25 +0000

    Moved renderer-specific code to examples/renderers/<renderer-name>/multiCurvePlot.py Changed name of output image file. Made completely pyvisi code, now no renderer-specific code.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@627 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/multiCurvePlotExample.py
+++ trunk/examples/multiCurvePlotExample.py
@@ -4,9 +4,6 @@
 Example of plotting multiple curves with pyvisi 
 """
 
-# what plotting method are we using?
-method = 'pyvisi'
-
 # set up some data to plot
 from Numeric import *
 
@@ -15,267 +12,40 @@ y1 = sin(x)
 y2 = cos(x)
 y3 = cos(x)**2
 
-# plot it using one of the three methods
-if method == 'pyvisi':
+# example code for how a user would write a script in pyvisi
+from pyvisi import *          # base level visualisation stuff
+# import the objects to render the scene using the specific renderer
+from pyvisi.renderers.gnuplot import *   # gnuplot
+#from pyvisi.renderers.vtk import *       # vtk
+#from pyvisi.renderers.plplot import *    # plplot
 
-    # example code for how a user would write a script in pyvisi
-    from pyvisi import *          # base level visualisation stuff
-    #from pyvisi.utils import *   # pyvisi specific utils
-    # import the objects to render the scene using the specific renderer
-    #from pyvisi.renderers.gnuplot import *   # gnuplot
-    #from pyvisi.renderers.vtk import *       # vtk
-    from pyvisi.renderers.plplot import *     # plplot
-    
-    # define the scene object
-    # a Scene is a container for all of the kinds of things you want to put 
-    # into your plot for instance, images, meshes, arrow/vector/quiver plots, 
-    # contour plots, spheres etc.
-    scene = Scene()
-    
-    # create a LinePlot object
-    plot = LinePlot(scene)
-    
-    # add some helpful info to the plot
-    plot.title = 'Example 2D plot'
-    plot.xlabel = 'x'
-    plot.ylabel = 'y'
+# define the scene object
+# a Scene is a container for all of the kinds of things you want to put 
+# into your plot for instance, images, meshes, arrow/vector/quiver plots, 
+# contour plots, spheres etc.
+scene = Scene()
 
-    plot.linestyle = 'lines'
-    
-    # assign some data to the plot
-    plot.setData(x, y1, y2, y3)
+# create a LinePlot object
+plot = LinePlot(scene)
 
-    # render the scene to screen
-    scene.render(pause=True,interactive=True)
+# add some helpful info to the plot
+plot.title = 'Example 2D plot'
+plot.xlabel = 'x'
+plot.ylabel = 'y'
 
-    # save the scene to file
-    plot.setData(x, y1, y2, y3)  # have to do this now because we've already
-                                 # render()ed the scene.  This requirement
-                                 # will be removed in the future.
-    scene.save(fname="multiCurvePlotExample.png", format=PngImage())
-    plot.setData(x, y1, y2, y3)  # have to do this now because we've already
-                                 # save()d the scene.  This requirement will
-                                 # be removed in the future.
-    scene.save(fname="multiCurvePlotExample.ps", format=PsImage())
+plot.linestyle = 'lines'
 
-elif method == 'gnuplot':
-    #### original gnuplot code
-    
-    import Gnuplot
+# assign some data to the plot
+plot.setData(x, y1, y2, y3)
 
-    # set the plot up
-    _gnuplot = Gnuplot.Gnuplot()
-    _gnuplot.title('Example 2D plot')
-    _gnuplot.xlabel('x')
-    _gnuplot.ylabel('y')
+# render the scene to screen
+scene.render(pause=True,interactive=True)
 
-    # set up the data
-    _data1 = Gnuplot.Data(x, y1, with='lines')
-    _data2 = Gnuplot.Data(x, y2, with='lines')
-    _data3 = Gnuplot.Data(x, y3, with='lines')
-
-    # plot it
-    _gnuplot.plot(_data1, _data2, _data3)
-
-    # save it to file
-    _gnuplot('set terminal png')
-    _gnuplot('set output "multiCurvePlotExample.png"')
-    _gnuplot.plot(_data1, _data2, _data3)
-
-    raw_input('Press enter to continue...\n')
-
-elif method == 'vtk':
-    #### original vtk code
-
-    import vtk
-
-    # set up the renderer and the render window
-    _ren = vtk.vtkRenderer()
-    _renWin = vtk.vtkRenderWindow()
-    _renWin.AddRenderer(_ren)
-    _renWin.SetSize(640, 480)
-
-    # do a quick check to make sure x and y are same length
-    if len(x) != len(y1):
-        raise ValueError, "x and y vectors must be same length"
-
-    if len(x) != len(y2):
-        raise ValueError, "x and y vectors must be same length"
-
-    # set up the x and y data arrays to be able to accept the data (code
-    # here adapted from the C++ of a vtk-users mailing list reply by Sander
-    # Niemeijer)
-    _xData = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
-    _xData.SetNumberOfTuples(len(x))
-
-    _yData1 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
-    _yData1.SetNumberOfTuples(len(y1))
-
-    _yData2 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
-    _yData2.SetNumberOfTuples(len(y2))
-
-    _yData3 = vtk.vtkDataArray.CreateDataArray(vtk.VTK_FLOAT)
-    _yData3.SetNumberOfTuples(len(y3))
-
-    # put the data into the data arrays
-    for i in range(len(x)):
-        _xData.SetTuple1(i,x[i])
-        _yData1.SetTuple1(i,y1[i])
-        _yData2.SetTuple1(i,y2[i])
-        _yData3.SetTuple1(i,y3[i])
-
-    # create a field data object 
-    # (I think this is as a containter to hold the data arrays)
-    _fieldData1 = vtk.vtkFieldData()
-    _fieldData1.AllocateArrays(2)
-    _fieldData1.AddArray(_xData)
-    _fieldData1.AddArray(_yData1)
-
-    _fieldData2 = vtk.vtkFieldData()
-    _fieldData2.AllocateArrays(2)
-    _fieldData2.AddArray(_xData)
-    _fieldData2.AddArray(_yData2)
-
-    _fieldData3 = vtk.vtkFieldData()
-    _fieldData3.AllocateArrays(2)
-    _fieldData3.AddArray(_xData)
-    _fieldData3.AddArray(_yData3)
-
-    # now put the field data object into a data object so that can add it as
-    # input to the xyPlotActor
-    _dataObject1 = vtk.vtkDataObject()
-    _dataObject1.SetFieldData(_fieldData1)
-
-    _dataObject2 = vtk.vtkDataObject()
-    _dataObject2.SetFieldData(_fieldData2)
-
-    _dataObject3 = vtk.vtkDataObject()
-    _dataObject3.SetFieldData(_fieldData3)
-
-    # set up the actor
-    _plot = vtk.vtkXYPlotActor()
-    _plot.AddDataObjectInput(_dataObject1)
-    _plot.AddDataObjectInput(_dataObject2)
-    _plot.AddDataObjectInput(_dataObject3)
-
-    # the size of the actor should be 80% of the render window
-    _plot.SetPosition(0.1, 0.1)  # (0.1 = (1.0 - 0.8)/2)
-    _plot.SetWidth(0.8)
-    _plot.SetHeight(0.8)
-
-    # set the title and stuff
-    _plot.SetTitle("Example 2D plot")
-    _plot.SetXTitle("x")
-    _plot.SetYTitle("y")
-    _plot.SetXValuesToValue()
-
-    #_titleProp = _plot.GetTitleTextProperty()
-    #_titleProp.SetJustificationToCentered()
-    #_titleProp.SetVerticalJustificationToBottom()
-
-     # set which parts of the data object are to be used for which axis
-    _plot.SetDataObjectXComponent(0, 0)
-    _plot.SetDataObjectYComponent(0, 1)
-    _plot.SetDataObjectYComponent(1, 1)
-    _plot.SetDataObjectYComponent(2, 1)
-
-    # set up the lookup table for the appropriate range of colours
-    _lut = vtk.vtkLookupTable()
-    _lut.Build()
-    _colours = []
-    for i in range(3):
-        _colours.append(_lut.GetColor(float(i)/float(3-1)))
-
-    # change the colour of the separate lines
-    _plot.SetPlotColor(0, _colours[0][0], _colours[0][1], _colours[0][2])
-    _plot.SetPlotColor(1, _colours[1][0], _colours[1][1], _colours[1][2])
-    _plot.SetPlotColor(2, _colours[2][0], _colours[2][1], _colours[2][2])
-
-    # change the plot style for the lines
-    #_plot.PlotLinesOff()
-    #_plot.PlotPointsOff()
-    #_plot.PlotCurvePointsOn()
-    #_plot.PlotCurveLinesOn()
-    #_plot.SetPlotPoints(0, 1)
-    #_plot.SetPlotPoints(1, 1)
-    #_plot.SetPlotLines(2, 1)
-    #print _plot
-    
-    _plot.GetXAxisActor2D().GetProperty().SetColor(0,0,0)
-    _plot.GetYAxisActor2D().GetProperty().SetColor(0,0,0)
-    _ren.SetBackground(1,1,1)
-
-    # add the actor
-    _ren.AddActor2D(_plot)
-    
-    # render the scene
-    _iren = vtk.vtkRenderWindowInteractor()
-    _iren.SetRenderWindow(_renWin)
-    _iren.Initialize()
-    _renWin.Render()
-    _iren.Start()
-
-    # convert the render window to an image
-    _win2imgFilter = vtk.vtkWindowToImageFilter()
-    _win2imgFilter.SetInput(_renWin)
-
-    # save the image to file
-    _outWriter = vtk.vtkPNGWriter()
-    _outWriter.SetInput(_win2imgFilter.GetOutput())
-    _outWriter.SetFileName("multiCurvePlotExample.png")
-    _outWriter.Write()
-
-    # pause for input
-    #raw_input('Press enter to continue...\n')
-
-elif method == "plplot":
-
-    import plplot
-
-    # determine the min and max of x
-    xMin = min(x)
-    xMax = max(x)
-    
-    # determine the global min and max of all the y's
-    yAll = concatenate( [y1, y2, y3] )
-
-    yMin = min(yAll)
-    yMax = max(yAll)
-
-    plplot.plsdev("xwin")
-    plplot.plinit()
-    plplot.plenv(xMin, xMax, yMin, yMax, 0, 1)
-    plplot.pllab("x", "y", "Example 2D plot")
-    plplot.plline(x, y1)
-    plplot.plline(x, y2)
-    plplot.plline(x, y3)
-    plplot.plend()
-
-    # to save as well, have to set everything up again, and replot
-    # save as png
-    plplot.plsdev("png")
-    plplot.plsfnam("multiCurvePlotExample.png")
-    plplot.plinit()
-    plplot.plenv(xMin, xMax, yMin, yMax, 0, 1)
-    plplot.pllab("x", "y", "Example 2D plot")
-    plplot.plline(x, y1)
-    plplot.plline(x, y2)
-    plplot.plline(x, y3)
-    plplot.plend()
-
-    # save as postscript
-    plplot.plsdev("psc")
-    plplot.plsfnam("multiCurvePlotExample.ps")
-    plplot.plinit()
-    plplot.plenv(xMin, xMax, yMin, yMax, 0, 1)
-    plplot.pllab("x", "y", "Example 2D plot")
-    plplot.plline(x, y1)
-    plplot.plline(x, y2)
-    plplot.plline(x, y3)
-    plplot.plend()
-
-else:
-    print "Eeek!  What plotting method am I supposed to use???"
+# save the scene to file
+plot.setData(x, y1, y2, y3)  # have to do this now because we've already
+                             # render()ed the scene.  This requirement
+                             # will be removed in the future.
+scene.save(fname="multiCurveLinePlot.png", format=PngImage())
 
 # vim: expandtab shiftwidth=4:
 
