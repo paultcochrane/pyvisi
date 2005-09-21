9f85d3db5e18bdac3ad4371464fd97890fdc4c44
Paul Cochrane <paultcochrane@gmail.com>
2005-09-21 00:40:16 +0000

    Dead wood removal.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@703 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/ellipsoidPlot.py
+++ trunk/examples/ellipsoidPlot.py
@@ -16,7 +16,6 @@ if method == 'pyvisi':
     # example code for how a user would write a script in pyvisi
     from pyvisi import *          # base level visualisation stuff
     # import the objects to render the scene using the specific renderer
-    #from pyvisi.renderers.gnuplot import *   # gnuplot
     from pyvisi.renderers.vtk import *       # vtk
     #from pyvisi.renderers.povray import *    # povray
     
