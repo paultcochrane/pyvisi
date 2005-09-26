835a98de89a5be2cd0fa7052b763581e248d36e2
Paul Cochrane <paultcochrane@gmail.com>
2005-09-26 04:20:28 +0000

    Fleshed out setDepth(), setBLF() and setTRB() methods.  Added debugMesg() calls, and added a render() method to the ClipBox object.  Refactored the tolerance setting to be an instance variable.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@724 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/pyvisi/renderers/vtk/box.py
+++ trunk/pyvisi/renderers/vtk/box.py
@@ -74,10 +74,14 @@ class Box(Item):
         self.blf = (self.xmin, self.ymin, self.zmin)
         self.trb = (self.xmax, self.ymax, self.zmax)
 
+        # tolerance for calculated variables checking purposes
+        self.tolerance = 1e-8
+
     def setBounds(self, xmin, xmax, ymin, ymax, zmin, zmax):
         """
         Set the bounds of the box
         """
+        debugMsg("Called Box.setBounds()")
         self.xmin = xmin
         self.xmax = xmax
         self.ymin = ymin
@@ -90,6 +94,7 @@ class Box(Item):
         """
         Get the current bounds of the box
         """
+        debugMsg("Called Box.getBounds()")
         return (self.xmin, self.xmax, \
                 self.ymin, self.ymax, \
                 self.zmin, self.zmax)
@@ -98,6 +103,7 @@ class Box(Item):
         """
         Set the origin of the box
         """
+        debugMsg("Called Box.setOrigin()")
         # get the current origin
         (xi, yi, zi) = self.getOrigin()
 
@@ -120,11 +126,10 @@ class Box(Item):
         # do a check to see if calculated origin is close enough to that
         # desired by the user (to within the tolerance)
         if __debug__:
-            tolerance = 1e-8
             (xi, yi, zi) = self.getOrigin()
             originDiff = (xo-xi, yo-yi, zo-zi)
             for i in range(3):
-                assert abs(originDiff[i]) < tolerance, \
+                assert abs(originDiff[i]) < self.tolerance, \
                         "Origin not set to within tolerance"
 
         return
@@ -133,12 +138,14 @@ class Box(Item):
         """
         Get the current origin of the box
         """
+        debugMsg("Called Box.getOrigin()")
         return self.origin
 
     def setWidth(self, width):
         """
         Set the width of the box
         """
+        debugMsg("Called Box.setWidth()")
         # get the current width
         oldWidth = self.getWidth()
 
@@ -160,9 +167,8 @@ class Box(Item):
 
         # do a check to make sure the calculated width is what was asked for
         if __debug__:
-            tolerance = 1e-8
             newWidth = self.getWidth()
-            assert abs(newWidth - width) < tolerance, \
+            assert abs(newWidth - width) < self.tolerance, \
                     "Width not set to within tolerance"
 
         return
@@ -171,38 +177,101 @@ class Box(Item):
         """
         Get the current box width
         """
+        debugMsg("Called Box.getWidth()")
         return self.width
 
     def setHeight(self, height):
         """
         Set the box height
         """
-        self.height = height
+        debugMsg("Called Box.setHeight()")
+        # get the current height
+        oldHeight = self.getHeight()
+
+        # get the current bounds
+        (xmin, xmax, ymin, ymax, zmin, zmax) = self.getBounds()
+
+        # add half the difference between the new height and the old height
+        # to the ymin and ymax variables
+        halfDiff = (height - oldHeight)/2.0
+
+        yminNew = ymin - halfDiff
+        ymaxNew = ymax + halfDiff
+
+        # reset the bounds
+        self.setBounds(xmin, xmax, yminNew, ymaxNew, zmin, zmax)
+
+        # set the height
+        self.height = ymaxNew - yminNew
+
+        # do a check to make sure the calculated height is what was asked
+        # for
+        if __debug__:
+            newHeight = self.getHeight()
+            assert abs(newHeight - height) < self.tolerance, \
+                    "Height not set to within tolerance"
+
         return
 
     def getHeight(self):
         """
         Get the current box height
         """
+        debugMsg("Called Box.getHeight()")
         return self.height
 
     def setDepth(self, depth):
         """
         Set the box depth
         """
-        self.depth = depth
+        debugMsg("Called Box.setDepth()")
+        # get the current depth
+        oldDepth = self.getDepth()
+
+        # get the current bounds
+        (xmin, xmax, ymin, ymax, zmin, zmax) = self.getBounds()
+
+        # add half the difference between the new depth and the old depth
+        # to the zmin and zmax variables
+        halfDiff = (depth - oldDepth)/2.0
+
+        zminNew = zmin - halfDiff
+        zmaxNew = zmax + halfDiff
+
+        # reset the bounds
+        self.setBounds(xmin, xmax, ymin, ymax, zminNew, zmaxNew)
+
+        # set the depth
+        self.depth = zmaxNew - zminNew
+
+        # do a check to make sure the calculated depth is what was asked
+        # for
+        if __debug__:
+            newDepth = self.getDepth()
+            assert abs(newDepth - depth) < self.tolerance, \
+                    "Depth not set to within tolerance"
+
         return
 
     def getDepth(self):
         """
         Get the current box depth
         """
+        debugMsg("Called Box.getDepth()")
         return self.depth
 
     def setBLF(self, bottom, left, front):
         """
         Set the position of the bottom, left, front corner
         """
+        debugMsg("Called Box.setBLF()")
+        # get the current bounds
+        (xmin, xmax, ymin, ymax, zmin, zmax) = self.getBounds()
+
+        # set the new bounds
+        self.setBounds(bottom, xmax, left, ymax, front, zmax)
+
+        # set the blf variable
         self.blf = (bottom, left, front)
         return
 
@@ -210,12 +279,21 @@ class Box(Item):
         """
         Get the current position of the bottom, left, front corner
         """
+        debugMsg("Called Box.getBLF()")
         return self.blf
 
     def setTRB(self, top, right, back):
         """
         Set the position of the top, right, back corner
         """
+        debugMsg("Called Box.setTRB()")
+        # get the current bounds
+        (xmin, xmax, ymin, ymax, zmin, zmax) = self.getBounds()
+
+        # set the new bounds
+        self.setBounds(xmin, top, ymin, right, zmin, back)
+
+        # set the trb variable
         self.trb = (top, right, back)
         return
 
@@ -223,6 +301,7 @@ class Box(Item):
         """
         Get the current position of the top, right, back corner
         """
+        debugMsg("Called Box.getTRB()")
         return self.trb
 
     def render(self):
@@ -230,6 +309,7 @@ class Box(Item):
         Perform Box object specific (pre)rendering tasks
         """
         debugMsg("Called Box.render()")
+        return
 
 
 class ClipBox(Box):
@@ -253,6 +333,7 @@ class ClipBox(Box):
         """
         Set the inside out flag
         """
+        debugMsg("Called ClipBox.setInsideOut()")
         self.insideOut = insideOut
         return
 
@@ -260,7 +341,16 @@ class ClipBox(Box):
         """
         Get the current value of the inside out flag
         """
+        debugMsg("Called ClipBox.getInsideOut()")
         return self.insideOut
+
+    def render(self):
+        """
+        Perform ClipBox object specific (pre)rendering tasks
+        """
+        debugMsg("Called ClipBox.render()")
+        return
+
 
 
 # vim: expandtab shiftwidth=4:
