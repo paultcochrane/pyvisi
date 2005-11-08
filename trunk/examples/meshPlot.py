3d8d10cdd527d8394c1d26cae391d41d187b449b
Paul Cochrane <paultcochrane@gmail.com>
2005-11-08 06:40:36 +0000

    Implemented passing renderer module as command line argument; default is gnuplot - need to implement the vtk version of this as well.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@752 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/meshPlot.py
+++ trunk/examples/meshPlot.py
@@ -4,8 +4,12 @@
 Example of plotting meshed surfaces with pyvisi 
 """
 
-# what plotting method are we using?
-method = 'pyvisi'
+import sys
+numArgs = len(sys.argv)
+if numArgs == 1:
+    ren_mod = "gnuplot"
+else:
+    ren_mod = sys.argv[1]
 
 # set up some data to plot
 from Numeric import *
@@ -24,52 +28,46 @@ for i in range(len(x)):
     for j in range(len(y)):
 	z[i,j] = x[i]*exp(-x[i]*x[i] - y[j]*y[j])
 
-# plot it with either gnuplot, vtk or pyvisi
-if method == 'pyvisi':
-    #### pyvisi version of code
-
-    # import the general pyvisi stuff
-    from pyvisi import *
-    # import the gnuplot overrides of the interface
+# import the general pyvisi stuff
+from pyvisi import *
+if ren_mod == "gnuplot":
     from pyvisi.renderers.gnuplot import *
-
-    # define a scene object
-    # a Scene is a container for all of the kinds of things you want to put
-    # into your plot, for instance, images, meshes, arrow/vector/quiver
-    # plots, contour plots, spheres etc.
-    scene = Scene()
-
-    # create a MeshPlot object
-    plot = MeshPlot(scene)
-
-    # add some helpful info to the plot
-    plot.title = 'Example mesh plot'
-    plot.xlabel = 'x'
-    plot.ylabel = 'y'
-    plot.zlabel = 'z'
-
-    # assign the data to the plot
-    # this version assumes that we have x, then y, then z and that z is 2D
-    # and that x and y are 1D arrays
-    plot.setData(x,y,z)
-    # alternative syntax
-    #plot.setData(xData=x, yData=y, zData=z)
-    # or (but more confusing depending upon one's naming conventions)
-    #plot.setData(x=x, y=y, z=z)
-
-    # render the scene to screen
-    scene.render(pause=True, interactive=True)
-
-    # save the scene to file
-    plot.setData(x,y,z)  # have to do this now because we've already
-                         # render()ed the scene.  This requirement will be
-                         # removed in the future
-    scene.save(fname="meshPlot.png", format=PngImage())
-
-elif method == 'vtk':
-    print "vtk mesh plotting not yet implemented"
-
+elif ren_mod == "vtk":
+    from pyvisi.renderers.vtk import *
 else:
-    print "Eeek!  What plotting method am I supposed to use???"
+    raise ValueError, "Unknown renderer module"
+
+# define a scene object
+# a Scene is a container for all of the kinds of things you want to put
+# into your plot, for instance, images, meshes, arrow/vector/quiver
+# plots, contour plots, spheres etc.
+scene = Scene()
+
+# create a MeshPlot object
+plot = MeshPlot(scene)
+
+# add some helpful info to the plot
+plot.title = 'Example mesh plot'
+plot.xlabel = 'x'
+plot.ylabel = 'y'
+plot.zlabel = 'z'
+
+# assign the data to the plot
+# this version assumes that we have x, then y, then z and that z is 2D
+# and that x and y are 1D arrays
+plot.setData(x,y,z)
+# alternative syntax
+#plot.setData(xData=x, yData=y, zData=z)
+# or (but more confusing depending upon one's naming conventions)
+#plot.setData(x=x, y=y, z=z)
+
+# render the scene to screen
+scene.render(pause=True, interactive=True)
+
+# save the scene to file
+plot.setData(x,y,z)  # have to do this now because we've already
+		     # render()ed the scene.  This requirement will be
+		     # removed in the future
+scene.save(fname="meshPlot.png", format=PngImage())
 
 # vim: expandtab shiftwidth=4:
