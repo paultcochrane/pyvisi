180f2c036192c7ed50f8713207fb41f5aaefc585
Paul Cochrane <paultcochrane@gmail.com>
2005-11-08 05:23:29 +0000

    Moved povray-specific code to it's own file in examples/renderers/povray. Added the ability to choose the renderer module from a command line argument to the script.  The default is vtk.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@744 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/arrowPlot3D.py
+++ trunk/examples/arrowPlot3D.py
@@ -4,8 +4,12 @@
 Example of plotting a 3D vector field with pyvisi 
 """
 
-# what plotting method are we using?
-method = 'pyvisi'
+import sys
+numArgs = len(sys.argv)
+if numArgs == 1:
+    ren_mod = "vtk"
+else:
+    ren_mod = sys.argv[1]
 
 # set up some data to plot
 from Numeric import *
@@ -36,194 +40,53 @@ for i in range(dim):
         dy[i,j] = (random.random()-0.5)/5.0
         dz[i,j] = (random.random()-0.5)/5.0
 
-# plot it using one of the three methods
-if method == 'pyvisi':
-
-    # example code for how a user would write a script in pyvisi
-    from pyvisi import *          # base level visualisation stuff
-    # import the objects to render the scene using the specific renderer
-    #from pyvisi.renderers.gnuplot import *   # gnuplot
+# example code for how a user would write a script in pyvisi
+from pyvisi import *          # base level visualisation stuff
+# import the objects to render the scene using the specific renderer
+#from pyvisi.renderers.gnuplot import *   # gnuplot
+if ren_mod == "vtk":
     from pyvisi.renderers.vtk import *       # vtk
-    #from pyvisi.renderers.povray import *    # povray
-    
-    # define the scene object
-    # a Scene is a container for all of the kinds of things you want to put 
-    # into your plot for instance, images, meshes, arrow/vector/quiver plots, 
-    # contour plots, spheres etc.
-    scene = Scene()
-    
-    # create a ArrowPlot3D object
-    plot = ArrowPlot3D(scene)
-    
-    # add some helpful info to the plot
-    plot.title = 'Example 3D arrow/quiver/vector field plot'
-    plot.xlabel = 'x'
-    plot.ylabel = 'y'
-    plot.zlabel = 'z'
-
-    # assign some data to the plot
-    plot.setData(x, y, z, dx, dy, dz)
-
-    # render the scene to screen
-    scene.render(pause=True, interactive=True)
-
-    # save the scene out to file
-    plot.setData(x, y, z, dx, dy, dz) # have to do this because we've already
-                               # render()ed the scene.  This requirement
-                               # will be removed in the future
-    scene.save(fname="arrowPlot3D.png", format=PngImage())
-
-    # plot data defined in a vtk file
-    plot.setData(fname='vel-0500.vtk', format='vtk-xml')
-
-    scene.render(pause=True, interactive=True)
-
-    # save this plot too
-    plot.setData(fname='vel-0500.vtk', format='vtk-xml')
-
-    scene.save(fname="arrowPlot3Dvtk.png", format="png")
-
-elif method == 'povray':
-    #### original povray code
-
-    import vtk
-    import os, sys, re, math
-    from Numeric import *
-    
-    # read in the file
-    reader = vtk.vtkXMLUnstructuredGridReader()
-    reader.SetFileName("vel-0500.vtk")
-    reader.Update()
-    
-    # get the grid
-    grid = reader.GetOutput()
-    
-    # grab the model centre and bounds
-    centre = grid.GetCenter()
-    bounds = grid.GetBounds()
-    
-    # try and extract the vector norm
-    norm = vtk.vtkVectorNorm()
-    norm.SetInput(grid)
-    
-    maxNorm = grid.GetPointData().GetVectors().GetMaxNorm()
-    
-    ### extract the relevant grid data
-    
-    # the points
-    points = grid.GetPoints()
-    numPoints = points.GetNumberOfPoints()
-    x = zeros(numPoints, typecode=Float)
-    y = zeros(numPoints, typecode=Float)
-    z = zeros(numPoints, typecode=Float)
-    for i in range(numPoints):
-        x[i], y[i], z[i] = points.GetPoint(i)
-    
-    # the data at the points
-    data = grid.GetPointData().GetVectors()
-    vx = zeros(numPoints, typecode=Float)
-    vy = zeros(numPoints, typecode=Float)
-    vz = zeros(numPoints, typecode=Float)
-    vNorm = zeros(numPoints, typecode=Float)
-    for i in range(numPoints):
-        vx[i], vy[i], vz[i] = data.GetTuple3(i)
-        vNorm[i] = math.sqrt(vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i])
-    
-    # make a lookup table for the colour map and invert it (colours look
-    # better when it's inverted)
-    lut = vtk.vtkLookupTable()
-    refLut = vtk.vtkLookupTable()
-    lut.Build()
-    refLut.Build()
-    for j in range(256):
-        lut.SetTableValue(j, refLut.GetTableValue(255-j))
-    
-    # get the colours
-    r = zeros(numPoints, typecode=Float)
-    g = zeros(numPoints, typecode=Float)
-    b = zeros(numPoints, typecode=Float)
-    for i in range(numPoints):
-        r[i], g[i], b[i] = lut.GetColor(vNorm[i]/maxNorm)
-    
-    ### generate the pov file
-    
-    pov = open("arrowPlot3D.pov", "w")
-    
-    pov.write("#include \"colors.inc\"\n")
-    pov.write("#include \"shapes.inc\"\n")
-    pov.write("#include \"textures.inc\"\n")
-    
-    pov.write("camera {\n")
-    pov.write("  location <%f, %f, -2.5>\n" % (centre[0], centre[1]))
-    pov.write("  look_at <%f, %f, -%f>\n" % (centre[0], centre[1], centre[2]))
-    pov.write("}\n")
-    
-    pov.write("light_source {\n")
-    pov.write("  <0, 0, -3>\n")
-    pov.write("  colour White\n")
-    pov.write("}\n")
-    
-    pov.write("#declare Arrow = union {\n")
-    pov.write("  cone {\n")
-    pov.write("    <0, 0, 0>, 0.3\n")
-    pov.write("    <1, 0, 0>, 0.0\n")
-    pov.write("  }\n")
-        
-    pov.write("  cylinder {\n")
-    pov.write("    <-1, 0, 0>\n")
-    pov.write("    <0, 0, 0>,\n")
-    pov.write("    0.15\n")
-    pov.write("  }\n")
-    pov.write("}\n")
-    
-    for i in range(numPoints):
-        pov.write("object {\n")
-        scale = 0.05*vNorm[i]/maxNorm
-        if scale < 1e-8:
-            scale = 1e-7
-        pov.write("  Arrow scale %g " % scale)
-        pov.write("rotate <%f, %f, %f> " % (vx[i], vy[i], vz[i]))
-        pov.write("translate <%f, %f, -%f> " % (x[i], y[i], z[i]))
-        pov.write("pigment { colour <%f, %f, %f> }\n" % (r[i], g[i], b[i]))
-        pov.write("}\n")
-    
-    pov.close()
-    
-    ### generate the ini file
-    
-    # open the ini file to write to
-    ini = open("arrowPlot3D.ini", "w")
-    
-    # the output resolution
-    ini.write("Width=640\n")
-    ini.write("Height=480\n")
-    
-    # anti-aliasing settings
-    ini.write("Antialias=on\n")
-    
-    # generate png files
-    ini.write("Output_File_Type=N\n")
-    
-    # the name of the input pov file
-    ini.write("Input_File_Name=arrowPlot3D.pov\n")
-    
-    # pause when done
-    ini.write("Pause_When_Done=on\n")
-    
-    # close the file
-    ini.close()
-    
-    # run povray on the file
-    result = os.system("povray arrowPlot3D.ini")
-    if result != 0:
-        raise SystemError, "Povray execution failed"
-    else:
-        # clean up a bit
-        os.unlink("arrowPlot3D.pov")
-        os.unlink("arrowPlot3D.ini")
- 
+elif ren_mod == "povray":
+    from pyvisi.renderers.povray import *    # povray
 else:
-    print "Eeek!  What plotting method am I supposed to use???"
+    raise ValueError, "Unknown renderer module"
+
+# define the scene object
+# a Scene is a container for all of the kinds of things you want to put 
+# into your plot for instance, images, meshes, arrow/vector/quiver plots, 
+# contour plots, spheres etc.
+scene = Scene()
+
+# create a ArrowPlot3D object
+plot = ArrowPlot3D(scene)
+
+# add some helpful info to the plot
+plot.title = 'Example 3D arrow/quiver/vector field plot'
+plot.xlabel = 'x'
+plot.ylabel = 'y'
+plot.zlabel = 'z'
+
+# assign some data to the plot
+plot.setData(x, y, z, dx, dy, dz)
+
+# render the scene to screen
+scene.render(pause=True, interactive=True)
+
+# save the scene out to file
+plot.setData(x, y, z, dx, dy, dz) # have to do this because we've already
+			   # render()ed the scene.  This requirement
+			   # will be removed in the future
+scene.save(fname="arrowPlot3D.png", format=PngImage())
+
+# plot data defined in a vtk file
+plot.setData(fname='vel-0500.vtk', format='vtk-xml')
+
+scene.render(pause=True, interactive=True)
+
+# save this plot too
+plot.setData(fname='vel-0500.vtk', format='vtk-xml')
+
+scene.save(fname="arrowPlot3Dvtk.png", format="png")
 
 # vim: expandtab shiftwidth=4:
 
