98b72db05f21234e11e94f8df74a88615653a352
Paul Cochrane <paultcochrane@gmail.com>
2005-11-08 06:03:33 +0000

    Implemented passing the renderer module in as a command line argument. Default is vtk.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@749 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/examples/ellipsoidPlot.py
+++ trunk/examples/ellipsoidPlot.py
@@ -4,48 +4,48 @@
 Example of plotting ellipsoids (useful for visualising tensors) with pyvisi 
 """
 
-# what plotting method are we using?
-method = 'pyvisi'
+import sys
+numArgs = len(sys.argv)
+if numArgs == 1:
+    ren_mod = "vtk"
+else:
+    ren_mod = sys.argv[1]
 
 # set up some data to plot
 from Numeric import *
 
-# plot it using one of the methods
-if method == 'pyvisi':
+# example code for how a user would write a script in pyvisi
+from pyvisi import *          # base level visualisation stuff
+# import the objects to render the scene using the specific renderer
 
-    # example code for how a user would write a script in pyvisi
-    from pyvisi import *          # base level visualisation stuff
-    # import the objects to render the scene using the specific renderer
+if ren_mod == "vtk":
     from pyvisi.renderers.vtk import *       # vtk
-    #from pyvisi.renderers.povray import *    # povray
-    
-    # define the scene object
-    # a Scene is a container for all of the kinds of things you want to put 
-    # into your plot for instance, images, meshes, arrow/vector/quiver plots, 
-    # contour plots, spheres etc.
-    scene = Scene()
-    
-    # create a EllipsoidPlot object
-    plot = EllipsoidPlot(scene)
-    
-    # add some helpful info to the plot
-    plot.title = 'Example ellipsoid plot'
-
-    # plot data defined in a vtk file
-    plot.setData(fname='stress22.vtk', format='vtk-xml')
-
-    scene.render(pause=True, interactive=True)
-
-    # save the plot
-    plot.setData(fname='stress22.vtk', format='vtk-xml')
-
-    scene.save(fname="ellipsoidPlot.png", format="png")
-
-elif method == 'povray':
-    print "Sorry, the povray interface hasn't been written yet."
-
+elif ren_mod == "povray":
+    from pyvisi.renderers.povray import *    # povray
 else:
-    print "Eeek!  What plotting method am I supposed to use???"
+    raise ValueError, "Unknown renderer module"
+
+# define the scene object
+# a Scene is a container for all of the kinds of things you want to put 
+# into your plot for instance, images, meshes, arrow/vector/quiver plots, 
+# contour plots, spheres etc.
+scene = Scene()
+
+# create a EllipsoidPlot object
+plot = EllipsoidPlot(scene)
+
+# add some helpful info to the plot
+plot.title = 'Example ellipsoid plot'
+
+# plot data defined in a vtk file
+plot.setData(fname='stress22.vtk', format='vtk-xml')
+
+scene.render(pause=True, interactive=True)
+
+# save the plot
+plot.setData(fname='stress22.vtk', format='vtk-xml')
+
+scene.save(fname="ellipsoidPlot.png", format="png")
 
 # vim: expandtab shiftwidth=4:
 
