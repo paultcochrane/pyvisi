097764cbac4990a33e7f15ca132580b8b6d86550
Paul Cochrane <paultcochrane@gmail.com>
2006-01-06 05:06:16 +0000

    This commit was manufactured by cvs2svn to create tag 'version_0_1_alpha_1'.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@815 4460aaa7-681d-0410-9b2c-fbf292dd6b46

---
+++ tags/version_0_1_alpha_1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# $Id$
+
+from distutils.core import setup
+
+a=setup(name="pyvisi",
+      version="0.1-alpha-1",
+      description="The Python Visualisation Interface",
+      author="Paul Cochrane",
+      author_email="paultcochrane@gmail.com",
+      url="http://pyvisi.sourceforge.net",
+      packages=['pyvisi',
+      'pyvisi.renderers',
+      'pyvisi.renderers.gnuplot',
+      'pyvisi.renderers.vtk',
+      ],
+)
+
+
