898592352c0b2fefbd6ece2830f2f48496b96321
Paul Cochrane <paultcochrane@gmail.com>
2006-01-06 03:37:36 +0000

    Fixed email address and updated version number.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@804 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/setup.py
+++ trunk/setup.py
@@ -5,10 +5,10 @@
 from distutils.core import setup
 
 a=setup(name="pyvisi",
-      version="0.1-pre-alpha-4",
+      version="0.1-alpha-1",
       description="The Python Visualisation Interface",
       author="Paul Cochrane",
-      author_email="cochrane@esscc.uq.edu.au",
+      author_email="paultcochrane@gmail.com",
       url="http://pyvisi.sourceforge.net",
       packages=['pyvisi',
       'pyvisi.renderers',
