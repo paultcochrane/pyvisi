ec8a9b874780e16fb4c876b596d7ab71f4c5f2a7
Paul Cochrane <paultcochrane@gmail.com>
2008-06-23 20:34:05 +0000

    Using a value of 8 for the vertical cut height.  This needs to be controlled from the makefile or somewhere else
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@922 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/special_vis/steffen/make_frames.py
+++ trunk/special_vis/steffen/make_frames.py
@@ -2,5 +2,6 @@
 
 import os
 
+
 for i in xrange(201):
-    os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -n 201" % (i,i))
+    os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -n 201 -v 8" % (i,i))
--- trunk/special_vis/steffen/make_frames_loop.py
+++ trunk/special_vis/steffen/make_frames_loop.py
@@ -5,6 +5,6 @@ import os
 for i in xrange(401):
     if i > 200:
 	index = 400 - i
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201" % (index,i))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8" % (index,i))
     else:
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201" % (i,i))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8" % (i,i))
