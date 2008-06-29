58c92d00f6a43e38a05d3d58f09ffcb1919c6a9e
Paul Cochrane <paultcochrane@gmail.com>
2008-06-29 08:15:11 +0000

    set the view radius to be 70 and the elevation angle to be -75.  this code is really beginning to smell.  it als really needs a config file structure rather than command line arguments
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@929 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/special_vis/steffen/make_frames.py
+++ trunk/special_vis/steffen/make_frames.py
@@ -4,4 +4,4 @@ import os
 
 
 for i in xrange(201):
-    os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -n 201 -v 8 -e -45" % (i,i))
+    os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -n 201 -v 8 -e -75 -r 70" % (i,i))
--- trunk/special_vis/steffen/make_frames_loop.py
+++ trunk/special_vis/steffen/make_frames_loop.py
@@ -5,6 +5,6 @@ import os
 for i in xrange(401):
     if i > 200:
 	index = 400 - i
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8 -e -45 " % (index,i))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8 -e -75 -r 70" % (index,i))
     else:
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8 -e -45" % (i,i))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8 -e -75 -r 70" % (i,i))
