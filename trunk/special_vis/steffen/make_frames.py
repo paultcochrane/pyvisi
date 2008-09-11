9e710970246bad5349a7d9625f4e7676ec017da6
Paul Cochrane <paultcochrane@gmail.com>
2008-09-11 20:44:35 +0000

    Added the ability to pass into make_frames.py the total number of frames.
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@930 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/special_vis/steffen/make_frames.py
+++ trunk/special_vis/steffen/make_frames.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 
 import os
+import getopt
+(opts, args) = getopt.getopt(sys.argv[1:],
+	"n:",
+	[
+	"numframes=", 
+	],
+	)
 
+numframes = None
+for option, arg in opts:
+    if option in ('-n', '--numframes'):
+	numframes = int(arg)
 
-for i in xrange(201):
-    os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -n 201 -v 8 -e -75 -r 70" % (i,i))
+if numframes is None:
+    raise ValueError, "You must supply the maximum number of frames"
+
+for i in xrange(numframes):
+    os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -n %i -v 8 -e -75 -r 70" % (i,i,numframes))
