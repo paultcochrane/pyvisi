713806196de4e0534d6c3bc1b28c60e1f0bd8d11
Paul Cochrane <paultcochrane@gmail.com>
2008-09-17 16:43:19 +0000

    Now passing the number of frames from the Makefile into the script
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@935 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/special_vis/steffen/make_frames_loop.py
+++ trunk/special_vis/steffen/make_frames_loop.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
-import os
+import os,sys
+import getopt
+(opts, args) = getopt.getopt(sys.argv[1:],
+	"n:",
+	[
+	"numframes=", 
+	],
+	)
 
-for i in xrange(401):
-    if i > 200:
-	index = 400 - i
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8 -e -75 -r 70" % (index,i))
+numframes = None
+for option, arg in opts:
+    if option in ('-n', '--numframes'):
+	numframes = int(arg)
+
+if numframes is None:
+    raise ValueError, "You must supply the maximum number of frames"
+
+totalframes = numframes*2 - 1
+for i in xrange(totalframes):
+    if i > numframes-1:
+	index = totalframes - 1 - i
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n %i -v 8 -e -75 -r 70" % (index,i,numframes))
     else:
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n 201 -v 8 -e -75 -r 70" % (i,i))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n %i -v 8 -e -75 -r 70" % (i,i,numframes))
