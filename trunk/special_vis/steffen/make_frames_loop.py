5c9f6402c41c7a9e926e0efc8cd528cf943d081c
Paul Cochrane <paultcochrane@gmail.com>
2008-09-22 20:02:16 +0000

    Using names instead of magic numbers
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@944 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/special_vis/steffen/make_frames_loop.py
+++ trunk/special_vis/steffen/make_frames_loop.py
@@ -18,9 +18,15 @@ if numframes is None:
     raise ValueError, "You must supply the maximum number of frames"
 
 totalframes = numframes*2 - 1
+vertical_cut_height = 0.0
+elevation_angle = 0.0
+view_radius = 100
 for i in xrange(totalframes):
     if i > numframes-1:
 	index = totalframes - 1 - i
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n %i -v 8 -e -75 -r 70" % (index,i,numframes))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i
+		-o vtk -s frame_loop -n %i -v %g -e %g -r %g" %
+		(index,i,numframes,vertical_cut_height,elevation_angle,view_radius))
     else:
-	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n %i -v 8 -e -75 -r 70" % (i,i,numframes))
+	os.system("python make_frame_povray.py -d vtk -f frame_%i.vtu -i %i -o vtk -s frame_loop -n %i -v %g -e %g -r %g" % 
+		(i,i,numframes,vertical_cut_height,elevation_angle,view_radius))
