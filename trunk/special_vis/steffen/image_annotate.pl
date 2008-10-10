a8bdea667a34faac5277c4ed458660112bd29e32
Paul Cochrane <paultcochrane@gmail.com>
2008-10-10 13:12:21 +0000

    Converted the image annotate program to only generate a single annotated image.  This means that the makefile can run in parallel
    git-svn-id: https://pyvisi.svn.sourceforge.net/svnroot/pyvisi@959 4460aaa7-681d-0410-9b2c-fbf292dd6b46

--- trunk/special_vis/steffen/image_annotate.pl
+++ trunk/special_vis/steffen/image_annotate.pl
@@ -9,13 +9,11 @@ use Getopt::Long;
 use Image::Magick;
 
 my $movie_title = "SimTitle";
-my $max_frames = 201;
-my $img_fname_stem = "frame";
+my $img_fname = "";
 
 my $result = GetOptions(
     "title=s" => \$movie_title,
-    "max_frames=i" => \$max_frames,
-    "stem=s" => \$img_fname_stem,
+    "fname=s" => \$img_fname,
 );
 
 my ($text_width, $text_height);
@@ -24,100 +22,102 @@ my $text_buffer_x = 5;
 my $text_buffer_y = -8;
 my @font_metrics;
 
-for (my $frame_num=0; $frame_num<$max_frames; $frame_num++) {
-    my $in_filename = sprintf("%s_%04d.tga", $img_fname_stem, $frame_num);
-    my $out_filename = sprintf("%s_%04d_ann.tga", $img_fname_stem, $frame_num);
+# work out the frame index and its stem from the input filename
+# the filename has to be in the form "<stem><number>.tga"
+$img_fname =~ m/(\w+)(\d+)\.tga/;
+my $img_fname_stem = $1;
+my $frame_index = $2;
+print $frame_index, "\n";
+my $in_filename = sprintf("%s%s.tga", $img_fname_stem, $frame_index);
+my $out_filename = sprintf("%s%s_ann.tga", $img_fname_stem, $frame_index);
 
-    # get the file
-    if ( ! -e $in_filename ) {
-	die "$in_filename does not exist!";
-    }
-    print "Reading $in_filename\n";
-    my $image = new Image::Magick;
-    $image->Read($in_filename);
-
-    # get image dimensions
-    my $image_height = $image->Get('height');
-    my $image_width = $image->Get('width');
-
-    # put a title on it
-    @font_metrics = $image->QueryFontMetrics(
-	text => $movie_title,
-	pointsize => 26,
-	font => $font,
-    );
-    my $movie_title_text_height = $font_metrics[5];
-
-    $image->Annotate(
-	text => $movie_title,
-	font => $font,
-	pointsize => 26,
-	stroke => 'white',
-	fill => 'white',
-	align => 'Center',
-	x => int $image_width/2,
-	y => $movie_title_text_height + $text_buffer_y - 5,
-    );
-
-    # put on it who made the data
-    my $info_text = "Steffen Abe";
-    @font_metrics = $image->QueryFontMetrics(
-	text => $info_text,
-	pointsize => 20,
-	font => $font,
-    );
-    my $info_text_height = $font_metrics[5];
-
-    $image->Annotate(
-	text => $info_text,
-	font => $font,
-	pointsize => 20,
-	stroke => 'white',
-	fill => 'white',
-	x => $text_buffer_x,
-	y => $info_text_height + $text_buffer_y,
-    );
-
-    # put on it who did the visualisation
-    my $vis_text = 'Visualisation: Paul Cochrane: paultcochrane@gmail.com';
-    $image->Annotate(
-	text => $vis_text,
-	font => $font,
-	pointsize => 14,
-	stroke => 'white',
-	fill => 'white',
-	align => 'Center',
-	x => int $image_width/2,
-	y => $image_height-13,
-    );
-
-    # determine the width of the frame number info text
-    my $frame_num_point_size = 18;
-    if ($frame_num == 0) {
-	my $frame_num_text = sprintf("frame: %03d", 0);
-	my @font_metrics = $image->QueryFontMetrics(
-	    text => $frame_num_text,
-	    pointsize => $frame_num_point_size,
-	    font => $font,
-	);
-	$text_width = $font_metrics[4];
-	$text_height = $font_metrics[5];
-    }
-
-    # put the frame number in the top right hand corner
-    my $frame_num_text = sprintf("frame: %03d", $frame_num);
-    $image->Annotate(
-	text => $frame_num_text,
-	font => $font,
-	pointsize => $frame_num_point_size,
-	stroke => 'white',
-	fill => 'white',
-	x => $image_width - $text_width - $text_buffer_x,
-	y => $text_height + $text_buffer_y,
-    );
-
-    # write out the result
-    $image->Write($out_filename);
-    print "Wrote $out_filename\n";
+# get the file
+if ( ! -e $in_filename ) {
+    die "$in_filename does not exist!";
 }
+print "Reading $in_filename\n";
+my $image = new Image::Magick;
+$image->Read($in_filename);
+
+# get image dimensions
+my $image_height = $image->Get('height');
+my $image_width = $image->Get('width');
+
+# put a title on it
+@font_metrics = $image->QueryFontMetrics(
+    text => $movie_title,
+    pointsize => 26,
+    font => $font,
+);
+my $movie_title_text_height = $font_metrics[5];
+
+$image->Annotate(
+    text => $movie_title,
+    font => $font,
+    pointsize => 26,
+    stroke => 'white',
+    fill => 'white',
+    align => 'Center',
+    x => int $image_width/2,
+    y => $movie_title_text_height + $text_buffer_y - 5,
+);
+
+# put on it who made the data
+my $info_text = "Steffen Abe";
+@font_metrics = $image->QueryFontMetrics(
+    text => $info_text,
+    pointsize => 20,
+    font => $font,
+);
+my $info_text_height = $font_metrics[5];
+
+$image->Annotate(
+    text => $info_text,
+    font => $font,
+    pointsize => 20,
+    stroke => 'white',
+    fill => 'white',
+    x => $text_buffer_x,
+    y => $info_text_height + $text_buffer_y,
+);
+
+# put on it who did the visualisation
+my $vis_text = 'Visualisation: Paul Cochrane: paultcochrane@gmail.com';
+$image->Annotate(
+    text => $vis_text,
+    font => $font,
+    pointsize => 14,
+    stroke => 'white',
+    fill => 'white',
+    align => 'Center',
+    x => int $image_width/2,
+    y => $image_height-13,
+);
+
+# determine the width of the frame index info text
+my $frame_index_point_size = 18;
+my $frame_index_text = sprintf("frame: %03d", 0);
+@font_metrics = $image->QueryFontMetrics(
+    text => $frame_index_text,
+    pointsize => $frame_index_point_size,
+    font => $font,
+);
+$text_width = $font_metrics[4];
+$text_height = $font_metrics[5];
+
+# put the frame index in the top right hand corner
+$frame_index_text = sprintf("frame: %03d", $frame_index);
+$image->Annotate(
+    text => $frame_index_text,
+    font => $font,
+    pointsize => $frame_index_point_size,
+    stroke => 'white',
+    fill => 'white',
+    x => $image_width - $text_width - $text_buffer_x,
+    y => $text_height + $text_buffer_y,
+);
+
+# write out the result
+$image->Write($out_filename);
+print "Wrote $out_filename\n";
 
